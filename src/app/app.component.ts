c230bee40d6c6b747f1eb943f339c0e5593234f7
Student <example@example.com>
2019-06-10 11:16:02 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+tasks: Task[] = [
+  new Task('Epicodus hw',3),
+  new Task('Dinner',2),
+  new Task('Cleaning',2)
+];
+  selectedTask: Task = this.tasks[0];
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  priorityColor(currentTask){
+  if (currentTask.priority === 3){
+    return "bg-danger";
+  } else if (currentTask.priority === 2) {
+    return  "bg-warning";
+  } else {
+    return "bg-info";
+  }
+}
 }
